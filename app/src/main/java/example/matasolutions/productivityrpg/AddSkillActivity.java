bf67e8bf1cba37221c07da00e39964f005a069a0
Your Name <you@example.com>
2019-09-17 20:27:02 +0300

    Basic skill functioning added

--- app/src/main/java/example/matasolutions/productivityrpg/AddSkillActivity.java
+++ app/src/main/java/example/matasolutions/productivityrpg/AddSkillActivity.java
@@ -3,6 +3,10 @@ package example.matasolutions.productivityrpg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddSkillActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class AddSkillActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_skill);
+
+        Button add_skill_button = findViewById(R.id.add_skill_button);
+        TextView skill_name_view = findViewById(R.id.skill_name);
+        TextView skill_multiplier_view = findViewById(R.id.skill_multiplier);
+
+        final String skill_name = skill_name_view.getText().toString();
+        String skill_multiplier = skill_multiplier_view.getText().toString();
+
+        final Double skill_multiplier_number = Double.parseDouble(skill_multiplier);
+
+        add_skill_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+
+
+            }
+        });
+
+
     }
 }
